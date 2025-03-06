7b2c40e32f3356fcb73e99920927bc5776650c0a
jgomezz <jgomezz@gmail.com>
2025-03-05 21:50:12 -0500

    update

---
+++ ejercicio_in.py
@@ -0,0 +1,13 @@
+lista_invitados = "Jose  Maria Meche Pedro"
+
+
+flag = "Maribel" in lista_invitados
+print(flag)
+
+
+flag = "Jose" in lista_invitados
+print(flag)
+
+
+flag = "Jaime" not in lista_invitados
+print(flag)
