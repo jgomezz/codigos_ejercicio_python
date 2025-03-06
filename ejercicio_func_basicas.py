a811ff4497b4f9932f0616c92ccd96f16c40c51d
jgomezz <jgomezz@gmail.com>
2025-03-05 21:29:41 -0500

    update

---
+++ ejercicio_func_basicas.py
@@ -0,0 +1,10 @@
+nombre = "Juan"
+apellido = "Garcia"
+
+# jgarcia@miempresa.com.pe
+
+patron = "@miempresa.com.pe"
+
+email = nombre[0] + apellido + patron
+
+print(email.lower())
---
+++ ejercicio_string.py
@@ -0,0 +1,13 @@
+
+
+msg = "Python es un lenguajez"
+
+
+print(msg[0])
+print(msg[7])
+
+print(msg[21])
+
+print(msg[-1])
+
+print(len(msg))
