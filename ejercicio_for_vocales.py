35ef982a1310a59d57200b1bcbbf3cb3f4509487
jgomezz <jgomezz@gmail.com>
2025-03-05 20:21:22 -0500

    first commit

---
+++ ejercicio_for_vocales.py
@@ -0,0 +1,17 @@
+#####################################
+#     Bucles : for
+#####################################
+nombre = "teofilo"
+
+#  a, e 
+cont_a = 0
+cont_e = 0
+
+for c in nombre:
+    if c == 'a':
+        cont_a = cont_a + 1
+    if c == 'e':
+            cont_e = cont_e + 1
+
+print(cont_a)
+print(cont_e)
