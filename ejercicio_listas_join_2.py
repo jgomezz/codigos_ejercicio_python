310695394274823b84ef76577e3e5f5b717eccea
jgomezz <jgomezz@gmail.com>
2025-03-10 21:32:11 -0500

    update

---
+++ ejercicio_listas_join_2.py
@@ -0,0 +1,15 @@
+# operacion split()
+frase = "Mi nuevo mensaje"
+print(frase)
+
+# Separados por " "
+palabras = frase.split(" ") 
+print(palabras)  # ['Mi', 'nuevo', 'mensaje']
+
+
+# operacion join()
+palabras = ['Mi', 'nuevo', 'mensaje']
+
+# Separados por " "
+frase = "-".join(palabras)
+print(frase)
