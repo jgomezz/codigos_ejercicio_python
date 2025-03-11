dd026c8288a9600b842c6a803fd06838c3f09c8b
jgomezz <jgomezz@gmail.com>
2025-03-10 20:49:05 -0500

    update

---
+++ ejercicio_list_tipo_datos.py
@@ -0,0 +1,16 @@
+valor = 10
+
+print(type(valor))
+
+if type(valor) == int :
+    print("Es entero")
+elif type(valor) == float :
+    print("Es deciaml")
+else : 
+    print("NO identificable")
+
+
+data = [45, 12.4, 1.13, 45, 4.5, 20, 1.8, 5.6, 14]
+
+
+# Calcular la suma de enteros y decimales
