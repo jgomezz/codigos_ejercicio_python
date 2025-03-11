a61a1af81d4bb5325db626960ba0d12034427d17
jgomezz <jgomezz@gmail.com>
2025-03-10 20:07:12 -0500

    update

--- ejercicios_tuplas.py
+++ ejercicios_tuplas.py
@@ -10,3 +10,10 @@ tamanho = len(estudiantes)
 print(estudiantes[tamanho-1])
 
 print(estudiantes[0::2])
+
+
+mi_tupla_1 = (3,4,5,6,7,8,9,10,11,12,13)
+
+nro_impares = mi_tupla_1[0::2]
+
+print(nro_impares)
