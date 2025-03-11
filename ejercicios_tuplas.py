f232b3fa2e1bec7399822f41fd6bba872140a2af
jgomezz <jgomezz@gmail.com>
2025-03-10 20:01:35 -0500

    update

--- ejercicios_tuplas.py
+++ ejercicios_tuplas.py
@@ -1,5 +1,5 @@
 #                 0        1       2
-estudiantes = ("Diego" ,"Jaime","Maribel", "Pedro")
+estudiantes = ("Diego" ,"Jaime","Maribel","Pedro","Virginia","Lucero")
 
 print(estudiantes[0])
 print(estudiantes[1])
@@ -8,3 +8,5 @@ print(estudiantes[-1])
 
 tamanho = len(estudiantes)
 print(estudiantes[tamanho-1])
+
+print(estudiantes[0::2])
