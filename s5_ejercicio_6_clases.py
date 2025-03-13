96fddbbbad1f84cf431ddfdd135b10f9d188b107
jgomezz <jgomezz@gmail.com>
2025-03-12 22:06:25 -0500

    update

--- s5_ejercicio_6_clases.py
+++ s5_ejercicio_6_clases.py
@@ -2,11 +2,20 @@
 class Humano:
     pass
 
-
 # Creacion de objetos
 Adan = Humano()
+Adan.papa = None
+Adan.mama = None
 
 Eva = Humano()
+Eva.papa = None
+Eva.mama = None
+
+Abel = Humano()
+Abel.papa = Adan
+Abel.mama =  Eva
+
+
 
 
 
