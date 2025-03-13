97dd2dcceb5d3e934b74d666c5014a2b068cd7ea
jgomezz <jgomezz@gmail.com>
2025-03-12 22:09:15 -0500

    update

--- s5_ejercicio_6_clases.py
+++ s5_ejercicio_6_clases.py
@@ -6,17 +6,22 @@ class Humano:
 Adan = Humano()
 Adan.papa = None
 Adan.mama = None
+Adan.edad = 33
 
 Eva = Humano()
 Eva.papa = None
 Eva.mama = None
+Eva.edad = 32
 
 Abel = Humano()
 Abel.papa = Adan
 Abel.mama =  Eva
+Abel.edad = 10
 
 
-
+print(Abel.mama.edad)
+print(Abel.papa.edad)
+print(Abel.edad)
 
 
 
