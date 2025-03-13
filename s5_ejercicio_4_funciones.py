27d9e6f5351ff93f3c597451f2adefb6c2b7a575
jgomezz <jgomezz@gmail.com>
2025-03-12 21:03:33 -0500

    update

---
+++ s5_ejercicio_4_funciones.py
@@ -0,0 +1,17 @@
+
+def imprimir_msg() :
+    print("Hola mundo")
+    print("Hola mundo")
+    print("Hola mundo")
+    print("Hola mundo")
+    print("Hola mundo")
+
+
+
+imprimir_msg()
+
+
+imprimir_msg()
+
+
+imprimir_msg()
