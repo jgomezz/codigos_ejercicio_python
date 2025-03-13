4f265370e7aa004b60c8f7a2a88d726e1d5265fa
jgomezz <jgomezz@gmail.com>
2025-03-12 20:47:16 -0500

    update

---
+++ s5_ejercicio_2_diccionarios.py
@@ -0,0 +1,19 @@
+
+participantes = {
+    1: "Jose",
+    2: "Alberto",
+    3: "Erick",
+    4: "Juana",
+    5: "Maria"
+}
+print(participantes[4])
+
+participantes[6] ="Patricia"
+
+print(participantes)
+
+print(participantes.keys())
+
+print(participantes.values())
+
+print(participantes.items())
---
+++ s5_ejercicio_3_diccionarios.py
@@ -0,0 +1,15 @@
+dias_semanas = {0:"Lunes",
+1:"Martes",
+2:"Miercoles",
+3:"Jueves",
+4:"Viernes",
+5:"Sabado,",
+6:"Domingo"}
+
+
+print(dias_semanas[0])
+print(dias_semanas[6])
+
+print("-----> Dias de la semana")
+for item in range(5):
+    print(dias_semanas[item])
