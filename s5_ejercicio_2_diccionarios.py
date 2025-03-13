20fef265eed147ab5c3230baa100dbbc2e68f8ce
jgomezz <jgomezz@gmail.com>
2025-03-12 20:54:27 -0500

    update

--- s5_ejercicio_2_diccionarios.py
+++ s5_ejercicio_2_diccionarios.py
@@ -17,3 +17,27 @@ print(participantes.keys())
 print(participantes.values())
 
 print(participantes.items())
+
+
+participantes = {
+    1: {
+        "nombre":"Jose",
+        "apellido":"Perez",
+        "edad":34
+        },
+    2: {
+        "nombre":"Alberto",
+        "apellido":"Garcia",
+        "edad":25
+        },
+    3: "Erick",
+    4: "Juana",
+    5: "Maria"
+}
+
+
+np1 = participantes[1]["nombre"]
+ep2 = participantes[2]["edad"]
+
+print(np1)
+print(ep2)
