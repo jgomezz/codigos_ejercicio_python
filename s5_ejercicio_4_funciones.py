248e750495052426190f7307f7ad83055da051b1
jgomezz <jgomezz@gmail.com>
2025-03-12 21:21:00 -0500

    update

--- s5_ejercicio_4_funciones.py
+++ s5_ejercicio_4_funciones.py
@@ -5,7 +5,6 @@ def imprimir_msg(nro_veces = 5) :
         print(f"Hola mundo {i+1}")
 
 
-
 imprimir_msg(5)
 
 imprimir_msg(12)
@@ -13,3 +12,30 @@ imprimir_msg(12)
 imprimir_msg(3)
 
 imprimir_msg()
+
+
+# Funcion de suma
+
+
+def suma (op1, op2):
+
+    s = op1 + op2
+
+    print(f"La suma es {s}")
+
+
+
+suma(13,12)
+suma(1,20)
+
+
+def resta (op1, op2):
+
+    r = op1 - op2
+
+    return r
+
+
+resultado = resta(13,12)
+
+print(resultado)
