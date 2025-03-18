95c838b90b1e31ccd097ce385ab6c32823e3035b
jgomezz <jgomezz@gmail.com>
2025-03-17 20:09:38 -0500

    update

---
+++ s6_ejercicio_1_class.py
@@ -0,0 +1,16 @@
+
+class Instituto:
+
+    def __init__(self, name , district):
+
+        self.name =  name
+        self.district =  district
+        
+
+# Programa Principal
+if __name__ == '__main__' :
+    
+    mi_instituto  = Instituto("Tecsup Centro","Santa Anita")
+
+    print(mi_instituto.name)
+
