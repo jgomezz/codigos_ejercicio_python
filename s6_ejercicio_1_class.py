37e88dc2f28da44027669d45823372264896994f
jgomezz <jgomezz@gmail.com>
2025-03-17 20:35:09 -0500

    update

--- s6_ejercicio_1_class.py
+++ s6_ejercicio_1_class.py
@@ -2,14 +2,22 @@
 class Instituto:
 
     def __init__(self, name , district):
-
         self.name =  name  # atributo nombre 
         self.district =  district
-        
 
     def getName(self):
         return self.name
-    
+
+
+class Universidad:
+
+    def __init__(self, nombre, sede, telefono, direccion, ruc):
+        self.nombre = nombre
+        self.sede = sede
+        self.telefono = telefono
+        self.direccion = direccion
+        self.ruc = ruc
+
 
 # Programa Principal
 if __name__ == '__main__' :
@@ -17,5 +25,8 @@ if __name__ == '__main__' :
     mi_instituto  = Instituto("Tecsup Centro","Santa Anita")
 
     print(mi_instituto.name)
-
     print(mi_instituto.getName())
+
+    mi_universidad = Universidad("UNI","Lima", 8333333, "Rimac", "1243347")
+
+    print(mi_universidad.nombre)
