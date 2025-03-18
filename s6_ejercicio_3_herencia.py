1552c90bef1e34fbfc201c665aa879f1337d61f4
jgomezz <jgomezz@gmail.com>
2025-03-17 21:30:52 -0500

    updateclear

--- s6_ejercicio_3_herencia.py
+++ s6_ejercicio_3_herencia.py
@@ -1,14 +1,4 @@
 
-class Instituto:
-
-    def __init__(self, name , district):
-        self.name =  name  # atributo nombre 
-        self.district =  district
-
-    def getName(self):
-        return self.name
-
-
 class Universidad:
 
     pais = "Peru"
@@ -24,16 +14,10 @@ class Universidad:
 # Programa Principal
 if __name__ == '__main__' :
     
-    mi_instituto  = Instituto("Tecsup Centro","Santa Anita")
-
-    print(mi_instituto.name)
-    print(mi_instituto.getName())
-
     mi_universidad = Universidad("UNI","Lima", 8333333, "Rimac", "1243347")
 
     print(mi_universidad.nombre)
     print(mi_universidad.pais)
-
 
     mi_universidad_2 = Universidad("San Marcoc","Lima", 1333333, "San Miguel", "12433427")
 
---
+++ s6_ejercicio_4_herencia.py
@@ -0,0 +1,13 @@
+
+class Auto:
+    pass
+
+class SUV (Auto):
+    pass
+    
+
+# Programa Principal
+if __name__ == '__main__' :
+
+    auto_SUV  = SUV()
+
