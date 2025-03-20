231ee4643c28aca3e9bfecb899ecccaf12a0d305
jgomezz <jgomezz@gmail.com>
2025-03-19 21:15:53 -0500

    updateclear

--- s7_script.sql
+++ s7_script.sql
@@ -15,3 +15,19 @@ CREATE TABLE employees (
 	hire_date date ,
 	PRIMARY KEY (emp_no)
 );
+
+DROP TABLE employees;
+
+
+
+CREATE TABLE comercio.employees (
+	emp_no int(11) NOT NULL AUTO_INCREMENT,
+	first_name varchar(14) NOT NULL,
+	last_name varchar(16) NOT NULL,
+	gender enum('H','M') NOT NULL,
+	birth_date date ,
+	hire_date date ,
+	PRIMARY KEY (emp_no)
+);
+
+DROP TABLE comercio.employees;
